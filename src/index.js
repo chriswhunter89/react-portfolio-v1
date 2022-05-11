61d692d39c44061229417b494b58b66d51f0a239
Chris Hunter <chriswhunter89@gmail.com>
2022-05-11 13:37:51 +0100

    initial commit and folder setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// react would typically be imported on this line but not required anymore
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
