422e6bb0a63015a74fa74ba6f139676e4dcd87b8
Chris Hunter <chriswhunter89@gmail.com>
2022-05-12 19:39:33 +0100

    footer created and styled

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,49 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
 import "./footer.css";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Chris Hunter
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/chris-hunter89/">
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/chriswhunter89">
+          <BsGithub />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; EGATOR Tutorials. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
